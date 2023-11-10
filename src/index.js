3e90b660e8f64356a88a7160f06668af74e5b5b0
Esila Adajewa <esila@esilas-macbook-air.home>
2023-11-10 21:22:34 +0100

    added basename do index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./scss/main.scss";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename='/prof-repairs'>
     <App />
   </BrowserRouter>
 );
